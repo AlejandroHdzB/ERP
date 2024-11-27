1aba0422179f4016b9fcaa7418e3daecf8c1b384
WallyRomSam <g3walteraromeros@gmail.com>
2024-11-27 01:03:39 -0600

    Adición de Controllers, DTOs y Services para Compras y Ventas

---
+++ src/main/java/com/cloudcomputing/erp/controllers/comprasVentas/compras/ReporteComprasController.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.cloudcomputing.erp.controllers.comprasVentas.compras;
+
+/**
+ *
+ * @author wolf_
+ */
+public class ReporteComprasController {
+
+}
---
+++ src/main/java/com/cloudcomputing/erp/controllers/comprasVentas/pedidos/ModificarEstatusPedidoController.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.cloudcomputing.erp.controllers.comprasVentas.pedidos;
+
+/**
+ *
+ * @author wolf_
+ */
+public class ModificarEstatusPedidoController {
+
+}
